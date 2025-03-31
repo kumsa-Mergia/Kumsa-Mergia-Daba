5864f0e12e405744bbda49c9203afbaec069fa07
Kumsa Mergia <kumsamergia@gmail.com>
2025-03-31 09:32:02 +0300

    Update Projects.jsx

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -11,13 +11,13 @@ const Projects = () => {
     {/* <div class="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8">  */}
      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-7'>
 
-     <div class="bg-white rounded-2xl shadow-md w-full">
+     <div class="bg-white rounded-2xl shadow-md w-full py-1">
       <img src={gemini} alt="Project Two" class="rounded-xl mb-4"/>
         <h2 class="text-xl font-semibold mb-2">Gemini Clone App</h2>
         <p className="text-gray-500">
         React application powered by the Gemini API, featuring a sleek Tailwind CSS interface. It allows users to interact with an AI chatbot, showcasing the integration of advanced language models in a responsive web environment.  
           </p>
-        <div class="flex justify-center space-x-4">
+        <div class="flex justify-center space-x-4 gap-2">
           <a href="https://github.com/kumsa-Mergia/gemini-clone" class="bg-black hover:bg-yellow-300 text-white font-semibold py-2 px-4 rounded">Github</a>
           <a href="https://clone-gemini-app.netlify.app/" class="bg-blue-500 hover:bg-yellow-300 text-white font-semibold py-2 px-4 rounded">Live Demo</a>
         </div>
